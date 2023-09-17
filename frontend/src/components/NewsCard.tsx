b2aef810d42fc0743abb757d2b4cc37235fe048f
mehmetali10 <mhmtali2316@gmail.com>
2023-09-17 22:48:19 +0300

    beuty newscard

--- frontend/src/components/NewsCard.tsx
+++ frontend/src/components/NewsCard.tsx
@@ -13,60 +13,57 @@ const newsCardStyles: Record<string, React.CSSProperties> = {
     flexDirection: 'column',
     marginBottom: '16px',
     backgroundColor: '#ffffff',
-    borderRadius: '20px',
-    minHeight: '350px',
+    borderRadius: '10px', 
+    minHeight: '320px', 
     width: '95%',
     margin: '0 auto',
-    marginTop: '55px',
-    boxShadow: '0 8px 16px rgba(0, 0, 0, 0.4)',
-    border: '1px solid #000', // Sınırı daha belirgin yapmak için buradaki değeri ekleyin
-  },
-  
-  
-  content: {
-    flex: '1 0 auto',
-    padding: '16px',
+    marginTop: '30px', 
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+    border: '1px solid gray', 
   },
   media: {
     width: '100%',
-    height: '400px',
+    height: '200px', 
     objectFit: 'cover',
-    marginBottom: '16px',
-    borderTopLeftRadius: '20px',
-    borderTopRightRadius: '20px',
+    borderTopLeftRadius: '10px', 
+    borderTopRightRadius: '10px',
+  },
+  content: {
+    flex: '1 0 auto',
+    padding: '16px',
   },
 };
 
 const NewsCard: React.FC<NewsCardProps> = ({ article }) => {
   return (
-      <Card style={newsCardStyles.card}>
-        {article.urlToImage && (
-          <CardMedia
-            style={newsCardStyles.media}
-            image={article.urlToImage}
-            title={article.title}
-          />
-        )}
-        <CardContent style={newsCardStyles.content}>
-          <Typography variant="h5" component="div" gutterBottom>
-            {article.title}
+    <Card style={newsCardStyles.card}>
+      {article.urlToImage && (
+        <CardMedia
+          style={newsCardStyles.media}
+          image={article.urlToImage}
+          title={article.title}
+        />
+      )}
+      <CardContent style={newsCardStyles.content}>
+        <Typography variant="h5" component="div" gutterBottom>
+          {article.title}
+        </Typography>
+        <Typography variant="body1" color="textPrimary">
+          {article.content?.substring(0, 200)}...
+          <Link href={article.url} target="_blank" rel="noopener" style={{ marginLeft: '4px', fontWeight: 'bold' }}>
+            Devamını Oku
+          </Link>
+        </Typography>
+        <div style={{ marginTop: '8px' }}>
+          <Typography variant="body2" color="textSecondary">
+            Published by {article.source.name}
           </Typography>
-          <Typography variant="body1" color="textPrimary">
-            {article.content?.substring(0, 250)}...
-            <Link href={article.url} target="_blank" rel="noopener" style={{ marginLeft: '4px', fontWeight: 'bold' }}>
-              Devamını Oku
-            </Link>
+          <Typography variant="body2" color="textSecondary">
+            Published on {new Date(article.publishedAt).toLocaleDateString()}
           </Typography>
-          <div style={{ marginTop: '16px' }}>
-            <Typography variant="body2" color="textSecondary">
-              Published by {article.source.name}
-            </Typography>
-            <Typography variant="body2" color="textSecondary">
-              Published on {new Date(article.publishedAt).toLocaleDateString()}
-            </Typography>
-          </div>
-        </CardContent>
-      </Card>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
